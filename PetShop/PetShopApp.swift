ffe5ca7c808c088f7c4c5e4b9d23f4dec96571d3
oliver33 <23cm0133@jec.ac.jp>
2024-02-17 00:20:46 +0900

    Initial Commit

---
+++ PetShop/PetShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetShopApp.swift
+//  PetShop
+//
+//  Created by OLIVER LIAO on 2024/2/17.
+//
+
+import SwiftUI
+
+@main
+struct PetShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
